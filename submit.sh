fb5f039e96d43dbcd9f13fd31ae23d109999b5ea
Maxim Vasilyev <mayxis@inbox.ru>
2024-02-23 15:35:57 +0300

    fix: add correct logic to submit script

--- submit.sh
+++ submit.sh
@@ -5,8 +5,16 @@ if [ "$#" -ne 2 ] ; then
     exit 1
 fi
 
+task=$2
+number=$(echo "$task" | sed 's/[^0-9]*//g')
+last_char="${task:0-1}"
+
 git add "$1"
-git commit -m "sln: add sln to $2"
+if [ $(printf '%s' "$last_char" | grep -c '[[:alpha:]]') -eq 1 ] ; then
+    git commit -m "sln: add sln to task $number $last_char"
+else
+    git commit -m "sln: add sln to $number"
+fi
 git push
 
 echo "Done!"
